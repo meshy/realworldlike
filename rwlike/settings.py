4fcddfe4ebcdbe1c870f41a84d7dfe70660035c1
Charlie Denton <charlie@meshy.co.uk>
2011-12-03 16:25:39 +0000

    updated settings for postgres

--- rwlike/settings.py
+++ rwlike/settings.py
@@ -13,11 +13,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'rwlike.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'rwlike',                      # Or path to database file if using sqlite3.
+        'USER': 'rwlike',                      # Not used with sqlite3.
+        'PASSWORD': 'rwlike',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
