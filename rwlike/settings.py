3ae4757bdc1f3a3fc174d6ccc03f6578877a37b3
Charlie Denton <charlie@meshy.co.uk>
2011-12-03 19:20:01 +0000

    changed static_media dir

--- rwlike/settings.py
+++ rwlike/settings.py
@@ -66,7 +66,7 @@ STATIC_ROOT = os.path.join(DIRNAME, 'static_media')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static_media/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
