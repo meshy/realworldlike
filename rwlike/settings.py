c2c71f6bc8e56b91a0b508e2f2b9657be3ee4e63
Charlie Denton <charlie@meshy.co.uk>
2011-12-04 10:11:55 +0000

    updates static paths

--- rwlike/settings.py
+++ rwlike/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = '/client_media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(DIRNAME, 'static_media')
+STATIC_ROOT = os.path.join(DIRNAME, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -71,7 +71,7 @@ STATIC_URL = '/static_media/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/static_media/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
