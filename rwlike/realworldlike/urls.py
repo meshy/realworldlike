418a6e0b50559db1cb47c3b26fe011c49fb3290c
Charlie Denton <charlie@meshy.co.uk>
2011-12-04 14:19:06 +0000

    remove CSRF protection on spot POST to allow SMS to post to us

--- rwlike/realworldlike/urls.py
+++ rwlike/realworldlike/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView, DetailView, ListView, TemplateView
 
 from realworldlike.models import PrintRun, Spot
@@ -13,5 +14,5 @@ urlpatterns = patterns('',
     url(r'^printrun/(?P<pk>\d+)/$', PrintRunJSONView.as_view(), name='printrun_json'),
     url(r'^spot/$', ListView.as_view(model=Spot), name='spot_list'),
     url(r'^spot/(?P<pk>\d+)/$', DetailView.as_view(model=Spot), name='spot_detail'),
-    url(r'^spot/new/$', SpotCreateView.as_view()),
+    url(r'^spot/new/$', csrf_exempt(SpotCreateView.as_view())),
 )
