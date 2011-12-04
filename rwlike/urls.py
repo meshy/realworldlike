b8c74c92fd8e011b7283953c46b6bebd17cbacbe
Charlie Denton <charlie@meshy.co.uk>
2011-12-04 12:29:22 +0000

    added local static

--- rwlike/urls.py
+++ rwlike/urls.py
@@ -1,6 +1,10 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
+
 
 admin.autodiscover()
 
@@ -10,3 +14,5 @@ urlpatterns = patterns('',
     url(r'^realworldlike/', include('realworldlike.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
